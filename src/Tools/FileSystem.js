d312b3cb19426f9cca301f5d8c4dcafb441b17d3
Hassan Salem <h.salem7788@gmail.com>
2018-12-25 21:50:43 +0200

    Initial initialization of FileSystem class

---
+++ src/Tools/FileSystem.js
@@ -0,0 +1,57 @@
+const FileExistsError = require('./Exceptions/FileExistsError.js')
+const FileNotFoundError = require('./Exceptions/FileNotFoundError.js')
+
+class FileSystem {
+
+    static putContent (path, content, recursive) {
+        if(this.exists(path)) {
+            return new FileExistsError('File exists')
+        }
+
+        try {
+            if(recursive) {
+                this.createDirectories(file)
+            }
+
+            fs.writeFileSync(path, content)
+        } catch (error) {
+            return new Error(error)
+        }
+
+        return true
+    }
+
+    static exists (file) {
+        return fs.existsSync(file)
+    }
+
+    static getContent (file) {
+        if(! this.exists(file)) {
+            return new FileNotFoundError
+        }
+
+        try {
+            return fs.readFileSync(file, 'utf8')
+        } catch (error) {
+            return null
+        }
+    }
+
+    static fileDirExists (path) {
+        let directory = path.dirname(file)
+        return fs.existsSync(directory)
+    }
+
+    static createDirectories (file) {
+        let directory = path.dirname(file)
+        let directories = directory.split(path.sep)
+        let step = ''
+        for (let dir of directories) {
+            step = step + dir + '/'
+            if(! fs.existsSync(step)) {
+                fs.mkdirSync(step)
+            }
+        }
+    }
+
+}
