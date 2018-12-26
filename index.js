cd701d4adf6aa4059a78a440006ab73561ace727
Hassan Salem <h.salem7788@gmail.com>
2018-12-26 10:14:33 +0200

    Add modules to export

---
+++ index.js
@@ -0,0 +1,9 @@
+const Command = require('./src/Command.js')
+const App = require('./src/App.js')
+const GeneratorCommand = require('./src/GeneratorCommand.js')
+
+module.exports = {
+    Command: Command,
+    App: App,
+    GeneratorCommand: GeneratorCommand
+}
