09380e2b980435b593e87fb78626c60d14758cf1
Hassan Salem <h.salem7788@gmail.com>
2018-12-26 08:53:18 +0200

    Implement Parser functionality

--- src/Tools/Parser.js
+++ src/Tools/Parser.js
@@ -1,5 +1,66 @@
 class Parser {
 
+    parse (input) {
+        this.input = input
+        this.matches = input.match(/\{\s*(.*?)\s*\}/g) || []
+
+        this.args = this.parseArgs(this.matches).args
+        this.options = this.parseArgs(this.matches).options
+        this.optionalArgs = this.parseArgs(this.matches).optionalArgs
+    }
+
+    parseArgs (matches) {
+        let args = []
+        let options = []
+        let optionalArgs = []
+
+        for(let arg of matches) {
+            if(this.isOption(arg)) {
+                options.push(this.cleanStr(arg))
+            } else {
+                if(this.isOptional(arg)) {
+                    optionalArgs.push(this.cleanStr(arg))
+                } else {
+                    args.push(this.cleanStr(arg))
+                }
+            }
+        }
+
+        return {
+            args: args,
+            optionalArgs: optionalArgs,
+            options: options
+        }
+    }
+
+    getName () {
+        return this.input.split(/ +/g)[0]
+    }
+
+    getArgs () {
+        return this.args
+    }
+
+    getOptionalArgs () {
+        return this.optionalArgs
+    }
+
+    getOptions () {
+        return this.options
+    }
+
+    isOptional (arg) {
+        return arg.indexOf('?') > 0
+    }
+
+    isOption (arg) {
+        return arg.indexOf('--') >= 0
+    }
+
+    // should be cleaner
+    cleanStr (str) {
+        return str.replace('{', '').replace('}', '').replace('--', '').replace('?', '')
+    }
 }
 
 module.exports = Parser
