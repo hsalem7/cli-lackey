d7b75175501079b1d0bebf8d7571cf9504c523cb
Hassan Salem <h.salem7788@gmail.com>
2018-12-26 09:13:46 +0200

    Implement init function and add the ability to inject the parser

--- src/Command.js
+++ src/Command.js
@@ -1,13 +1,26 @@
 const Log = require('./../src/Tools/Log.js')
+const Parser = require('./../src/Tools/Parser.js')
 
 class Command {
 
-    constructor (logger = null) {
+    constructor (logger = null, parser = null) {
         this.logger = logger || Log
+        this.parser = parser || Parser
+        this.init()
+    }
+
+    init () {
+        let parser = new Parser()
+        parser.parse(this.getSignature())
+
+        this.name = parser.getName()
+        this.args = parser.getArgs()
+        this.optionalArgs = parser.getOptionalArgs()
+        this.options = parser.getOptions()
     }
 
     getSignature () {
-
+        return this.config().signature
     }
 
     info (...message) {
