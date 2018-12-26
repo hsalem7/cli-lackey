192e4a3409f838f7ab72f367bdb1ea43eeb8b7e1
Hassan Salem <h.salem7788@gmail.com>
2018-12-26 11:11:02 +0200

    Add user registered commands

--- bin/lackey.js
+++ bin/lackey.js
@@ -2,9 +2,12 @@
 
 const App = require('./../src/App.js')
 const CreateCommand = require('./../examples/CreateCommand.js')
+const commands = require(process.cwd() + '/commands.js')
 
 let app = new App
 
 app.register(new CreateCommand)
+
+app.registerAll(commands)
 
 app.run()
