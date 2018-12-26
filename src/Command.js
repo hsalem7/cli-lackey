9fa426920008308db67936fa5cad0d4723d4b1c3
Hassan Salem <h.salem7788@gmail.com>
2018-12-26 08:56:19 +0200

    Create Command class and add some empty functions to it

---
+++ src/Command.js
@@ -0,0 +1,28 @@
+class Command {
+
+    constructor () {
+
+    }
+
+    getSignature () {
+
+    }
+
+    info (message) {
+
+    }
+
+    error (message) {
+
+    }
+
+    warn (message) {
+
+    }
+
+    output (text, color) {
+
+    }
+}
+
+module.exports = Command
